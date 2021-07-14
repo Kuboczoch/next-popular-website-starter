97e092c6abc074daf65df0a03776eb29437731e6
Bartlomiej <bartlomiejwendt@gmail.com>
2021-07-14 21:13:57 +0200

    :sparkles: Add Lighthouse Github action

--- src/containers/HomePage/HomePageView.tsx
+++ src/containers/HomePage/HomePageView.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-import { IHomePageProps } from '../../pages/index'
+import { IHomePageProps } from '../../pages'
 import { IHomePageStateProps } from './useHomePage'
 
 import HomePage from '../../components/blocks/HomePage'
 
 interface IHomePageViewProps extends IHomePageProps, IHomePageStateProps {}
 
-const HomePageView = ({}: IHomePageViewProps) => <HomePage />
+const HomePageView = ({}: IHomePageViewProps) => (
+  <HomePage>
+    <h1>Hello my awesome template!</h1>
+  </HomePage>
+)
 
 export default HomePageView
