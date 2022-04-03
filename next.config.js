d9c388bf6474c10639c8b9c79940c8610f3f11ca
Kuboczoch (Jakub Pokorski) <jakub.pokorski@protonmail.com>
2022-04-03 14:44:21 +0200

    :arrow_up: Bump dependencies, added swc

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['images.ctfassets.net'],
   },
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
 }
