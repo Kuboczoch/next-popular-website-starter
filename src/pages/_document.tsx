dded576b11dfdf69b69c9c8a2c4e16e558fd7269
Kuboczoch (Jakub Pokorski) <jakub.pokorski@protonmail.com>
2022-04-12 22:38:34 +0200

    :art: Added viewport #9

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,10 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
             rel='stylesheet'
           />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1, maximum-scale=1'
+          />
         </Head>
         <body>
           <Main />
